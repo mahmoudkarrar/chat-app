f284a173d89786f9689d3097beaa91a83a5b350f
mahmoudkarrar <mahmod.fathi@gmail.com>
2021-11-15 15:03:55 +0200

    Adding websocket basic configurations and client for quick test (#1)
    Co-authored-by: Mahmoud Karrar <mahmoud.karrar@klivvr.com>

---
+++ src/main/java/com/chatapp/config/WebSocketConfig.java
@@ -0,0 +1,19 @@
+package com.chatapp.config;
+
+import com.chatapp.handler.ChatWebSocketHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+
+import java.util.Map;
+
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public HandlerMapping webSocketMapping(ChatWebSocketHandler webSocketHandler) {
+        return new SimpleUrlHandlerMapping(Map.of("/chat", webSocketHandler), -1);
+    }
+
+
+}
