e339bf3ac1d356f3b3d8ae42185fc6c32a212162
Mahmoud Karrar <mahmoud.karrar@klivvr.com>
2021-11-17 00:08:52 +0200

    enable broadcasting messages using flux message channel

--- src/main/java/com/chatapp/config/WebSocketServerConfig.java
+++ src/main/java/com/chatapp/config/WebSocketServerConfig.java
@@ -3,16 +3,21 @@ package com.chatapp.config;
 import com.chatapp.handler.ChatWebSocketHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.channel.FluxMessageChannel;
 import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
 
 import java.util.Map;
 
 @Configuration
-public class WebSocketConfig {
+public class WebSocketServerConfig {
     @Bean
     public HandlerMapping webSocketMapping(ChatWebSocketHandler webSocketHandler) {
         return new SimpleUrlHandlerMapping(Map.of("/chat", webSocketHandler), -1);
     }
 
+    @Bean
+    public FluxMessageChannel fluxMessageChannel() {
+        return new FluxMessageChannel();
+    }
 }
