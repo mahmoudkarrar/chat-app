8061b630cea048bd244154bab2b9cd0cf20537c2
Mahmoud Karrar <mahmoud.karrar@klivvr.com>
2021-11-16 23:53:17 +0200

    test multiple messages

--- src/test/java/com/chatapp/handler/ChatWebSocketHandlerIntegrationTest.java
+++ src/test/java/com/chatapp/handler/ChatWebSocketHandlerIntegrationTest.java
@@ -16,11 +16,13 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
 import org.springframework.web.reactive.socket.client.WebSocketClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 import reactor.test.StepVerifier;
 
 import java.net.URI;
+import java.time.Duration;
 
 import static java.lang.String.format;
 
@@ -34,7 +36,8 @@ class ChatWebSocketHandlerIntegrationTest {
     @LocalServerPort
     private String port;
 
-    private final Event TEST_PAYLOAD = Event.builder().type(EventType.CHAT).content("welcome to the best chat!")
+    private final String CHAT_MSG_CONTENT = "Test Msg";
+    private final Event TEST_PAYLOAD = Event.builder().type(EventType.CHAT).content(CHAT_MSG_CONTENT)
             .sender("test").build();
 
     private ObjectMapper objectMapper = new ObjectMapper();
@@ -64,4 +67,39 @@ class ChatWebSocketHandlerIntegrationTest {
     private String toString(Event event) {
         return objectMapper.writeValueAsString(event);
     }
+
+    @SneakyThrows
+    private Event toEvent(String eventString) {
+        return objectMapper.readValue(eventString, Event.class);
+    }
+    @Test
+    void shouldBroadcastAllMessagesToTheClient() {
+        //clients
+        WebSocketClient client = new ReactorNettyWebSocketClient();
+
+        String msgToBeSent = toString(TEST_PAYLOAD);
+        Sinks.Many<String> sink = Sinks.many().multicast().onBackpressureBuffer();
+        int count = 4;
+        Flux<String> input = Flux.range(1, count).map(index -> msgToBeSent.replace(CHAT_MSG_CONTENT, CHAT_MSG_CONTENT+ index));
+
+        client.execute(URI.create(format("ws://localhost:%s/chat", port)),
+                        session -> session.send(input.map(session::textMessage))
+                                .thenMany(
+                                        session.receive().take(count).map(WebSocketMessage::getPayloadAsText)
+                                                .doOnNext(sink::tryEmitNext)
+                                                .then())
+                                .then())
+                .block(Duration.ofSeconds(1));
+
+
+        StepVerifier.create(sink.asFlux().take(count).map(this::toEvent).map(Event::getContent))
+                .expectNext(CHAT_MSG_CONTENT + 1)
+                .expectNext(CHAT_MSG_CONTENT + 2)
+                .expectNext(CHAT_MSG_CONTENT + 3)
+                .expectNext(CHAT_MSG_CONTENT + 4)
+                .verifyComplete();
+
+
+    }
+
 }
