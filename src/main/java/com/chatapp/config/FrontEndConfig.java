4cbc44f4945a183a2488f708bd84b00c2d840352
Mahmoud Karrar <mahmoud.karrar@klivvr.com>
2021-11-17 07:20:08 +0200

    feature add frontend pages

---
+++ src/main/java/com/chatapp/config/FrontEndConfig.java
@@ -0,0 +1,38 @@
+package com.chatapp.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@Configuration
+public class FrontEndConfig {
+    @Bean
+    public RouterFunction<ServerResponse> indexRouter(@Value("classpath:/index.html") final Resource indexHtml){
+        return  route(GET("/"), request -> ok().contentType(MediaType.TEXT_HTML).bodyValue(indexHtml));
+    }
+    @Bean
+    public RouterFunction<ServerResponse> cssRouter() {
+        return RouterFunctions
+                .resources("/css/**", new ClassPathResource("css/"));
+    }
+    @Bean
+    public RouterFunction<ServerResponse> jsRouter() {
+        return RouterFunctions
+                .resources("/js/**", new ClassPathResource("js/"));
+    }
+    @Bean
+    public RouterFunction<ServerResponse> imgRouter() {
+        return RouterFunctions
+                .resources("/img/**", new ClassPathResource("img/"));
+    }
+}
---
+++ src/main/resources/js/main.js
@@ -0,0 +1,128 @@
+'use strict';
+
+const usernamePage = document.querySelector('#username-page');
+const chatPage = document.querySelector('#chat-page');
+const usernameForm = document.querySelector('#usernameForm');
+const messageForm = document.querySelector('#messageForm');
+const messageInput = document.querySelector('#message');
+const messageArea = document.querySelector('#messageArea');
+const connectingElement = document.querySelector('.connecting');
+
+let username = null;
+let socket = null;
+
+const colors = [
+    '#2196F3', '#32c787', '#00BCD4', '#ff5652',
+    '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
+];
+
+function connect(event) {
+    username = document.querySelector('#name').value.trim();
+
+    if(username) {
+        usernamePage.classList.add('hidden');
+        chatPage.classList.remove('hidden');
+
+        socket = new WebSocket("ws://localhost:8080/chat");
+
+        socket.onopen = function() {
+
+            socket.send(JSON.stringify({sender: username, type: 'JOIN'}))
+
+            connectingElement.classList.add('hidden');
+        };
+
+        socket.onmessage = function(event) {
+            onMessageReceived(event.data);
+        };
+
+        socket.onclose = function(event) {
+            username = null;
+            if (event.wasClean) {
+                console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
+            } else {
+                // e.g. server process killed or network down
+                // event.code is usually 1006 in this case
+                alert('[close] Connection died');
+            }
+        };
+
+        socket.onerror = function(error) {
+            console.error(`[error] ${error.message}`);
+            connectingElement.textContent = 'Unable to connect to the server! Please refresh the page and try again.';
+            connectingElement.style.color = 'red';
+        }
+
+    }
+    event.preventDefault();
+}
+
+
+function send(event) {
+    const messageContent = messageInput.value.trim();
+
+    if (messageContent) {
+        const chatMessage = {
+            sender: username,
+            content: messageInput.value,
+            type: 'CHAT'
+        };
+
+        socket.send(JSON.stringify(chatMessage));
+        messageInput.value = '';
+        event.preventDefault();
+    }
+}
+
+
+function onMessageReceived(payload) {
+
+    const message = JSON.parse(payload);
+
+    const messageElement = document.createElement('li');
+
+    if(message.type === 'JOIN') {
+        messageElement.classList.add('event-message');
+        message.content = message.sender + ' joined!';
+    } else if (message.type === 'LEAVE') {
+        messageElement.classList.add('event-message');
+        message.content = message.sender + ' left!';
+    } else {
+        messageElement.classList.add('chat-message');
+
+        const avatarElement = document.createElement('i');
+        const avatarText = document.createTextNode(message.sender[0]);
+        avatarElement.appendChild(avatarText);
+        avatarElement.style['background-color'] = getAvatarColor(message.sender);
+
+        messageElement.appendChild(avatarElement);
+
+        const usernameElement = document.createElement('span');
+        const usernameText = document.createTextNode(message.sender);
+        usernameElement.appendChild(usernameText);
+        messageElement.appendChild(usernameElement);
+    }
+
+    const textElement = document.createElement('p');
+    const messageText = document.createTextNode(message.content);
+    textElement.appendChild(messageText);
+
+    messageElement.appendChild(textElement);
+
+    messageArea.appendChild(messageElement);
+    messageArea.scrollTop = messageArea.scrollHeight;
+}
+
+
+function getAvatarColor(messageSender) {
+    let hash = 0;
+    for (let i = 0; i < messageSender.length; i++) {
+        hash = 31 * hash + messageSender.charCodeAt(i);
+    }
+
+    const index = Math.abs(hash % colors.length);
+    return colors[index];
+}
+
+usernameForm.addEventListener('submit', connect, true)
+messageForm.addEventListener('submit', send, true)
