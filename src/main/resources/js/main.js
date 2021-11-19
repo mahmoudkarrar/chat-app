245e14e369748ed782c02110fbbdebd165bb0bca
mahmoudkarrar <mahmod.fathi@gmail.com>
2021-11-19 23:57:03 +0200

    implement search in chat (#9)

--- src/main/resources/js/main.js
+++ src/main/resources/js/main.js
@@ -7,46 +7,43 @@ const messageForm = document.querySelector('#messageForm');
 const messageInput = document.querySelector('#message');
 const messageArea = document.querySelector('#messageArea');
 const connectingElement = document.querySelector('.connecting');
+const searchText = document.querySelector('#search');
 
 let username = null;
 let socket = null;
+
 
 const colors = [
     '#2196F3', '#32c787', '#00BCD4', '#ff5652',
     '#ffc107', '#ff85af', '#FF9800', '#39bbb0'
 ];
 
-function connect(event) {
+const connect = (event) => {
     username = document.querySelector('#name').value.trim();
 
-    if(username) {
+    if (username) {
         usernamePage.classList.add('hidden');
         chatPage.classList.remove('hidden');
 
         socket = new WebSocket("ws://localhost:8080/chat");
 
-        socket.onopen = function() {
+        socket.onopen = () => {
             loadHistory();
-
             connectingElement.classList.add('hidden');
         };
 
-        socket.onmessage = function(event) {
-            onMessageReceived(event.data);
-        };
+        socket.onmessage = (event) => onMessageReceived(event.data);
 
-        socket.onclose = function(event) {
+        socket.onclose = (event) => {
             username = null;
             if (event.wasClean) {
                 console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
             } else {
-                // e.g. server process killed or network down
-                // event.code is usually 1006 in this case
                 alert('[close] Connection died');
             }
         };
 
-        socket.onerror = function(error) {
+        socket.onerror =  (error) => {
             console.error(`[error] ${error.message}`);
             connectingElement.textContent = 'Unable to connect to the server! Please refresh the page and try again.';
             connectingElement.style.color = 'red';
@@ -57,8 +54,8 @@ function connect(event) {
 }
 
 
-function send(event) {
-    const messageContent = messageInput.value.trim();
+const send = (event) => {
+    const messageContent = messageInput.value;
 
     if (messageContent) {
         const chatMessage = {
@@ -74,15 +71,15 @@ function send(event) {
 }
 
 
-function onMessageReceived(payload) {
+const onMessageReceived = (payload) => {
     const message = JSON.parse(payload);
     renderMessage(message);
 }
 
-function renderMessage(message) {
+const renderMessage = (message) => {
     const messageElement = document.createElement('li');
 
-    if(message.type === 'JOIN') {
+    if (message.type === 'JOIN') {
         messageElement.classList.add('event-message');
         message.content = message.sender + ' joined!';
     } else if (message.type === 'LEAVE') {
@@ -94,7 +91,7 @@ function renderMessage(message) {
         const avatarElement = document.createElement('i');
         const avatarText = document.createTextNode(message.sender[0]);
         avatarElement.appendChild(avatarText);
-        avatarElement.style['background-color'] = getAvatarColor(message.sender);
+        avatarElement.style['background-color'] = getAvatarColor(getHash(message.sender));
 
         messageElement.appendChild(avatarElement);
 
@@ -114,32 +111,36 @@ function renderMessage(message) {
     messageArea.scrollTop = messageArea.scrollHeight;
 }
 
-function getAvatarColor(messageSender) {
+const getHash = (messageSender) => {
     let hash = 0;
     for (let i = 0; i < messageSender.length; i++) {
         hash = 31 * hash + messageSender.charCodeAt(i);
     }
-
+    return hash;
+}
+const getAvatarColor = (hash) => {
     const index = Math.abs(hash % colors.length);
     return colors[index];
 }
 
- function loadHistory() {
-     axios.get('/api/v1/chat/history')
-         .then(function (response) {
-             console.log("success "+response);
-             response.data.forEach( element => {
-                 renderMessage(element)
-             });
-         })
-         .catch(function (error) {
-             console.log(error);
-         })
-         .then(function () {
-             socket.send(JSON.stringify({sender: username, type: 'JOIN'}))
-         });
-    }
+const loadHistory = () => axios.get('/api/v1/chat/history')
+    .then(response => response.data.forEach(element => renderMessage(element)))
+    .catch(err => console.log(err))
+    .then(() => socket.send(JSON.stringify({sender: username, type: 'JOIN'})));
 
+searchText.addEventListener('keyup', (ev) => {
+    let text = ev.target.value;
+    let pat = new RegExp(text, 'i');
+
+    for(const element of messageArea.children) {
+        if (pat.test(element.innerText)) {
+            element.classList.remove("hidden");
+        } else {
+            element.classList.add("hidden");
+        }
+
+    }
+});
 
 usernameForm.addEventListener('submit', connect, true)
 messageForm.addEventListener('submit', send, true)
