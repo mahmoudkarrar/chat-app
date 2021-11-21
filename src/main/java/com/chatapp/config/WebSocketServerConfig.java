9b850080c86ad58889c71fd0f834f860afbc041c
Mahmoud Karrar <mahmoud.karrar@klivvr.com>
2021-11-21 18:48:50 +0200

    feature add generic errorChannel

--- src/main/java/com/chatapp/config/WebSocketServerConfig.java
+++ src/main/java/com/chatapp/config/WebSocketServerConfig.java
@@ -1,15 +1,18 @@
 package com.chatapp.config;
 
 import com.chatapp.handler.ChatWebSocketHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.channel.FluxMessageChannel;
+import org.springframework.integration.channel.PublishSubscribeChannel;
 import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
 
 import java.util.Map;
 
 @Configuration
+@Slf4j
 public class WebSocketServerConfig {
     @Bean
     public HandlerMapping webSocketMapping(ChatWebSocketHandler webSocketHandler) {
@@ -19,5 +22,13 @@ public class WebSocketServerConfig {
     @Bean
     public FluxMessageChannel fluxMessageChannel() {
         return new FluxMessageChannel();
+    }
+
+    @Bean
+    public PublishSubscribeChannel errorChannel() {
+        PublishSubscribeChannel channel = new PublishSubscribeChannel(true);
+        channel.setBeanName("errorChannel");
+        channel.subscribe(message -> log.error("ERROR detected {}",message));
+        return channel;
     }
 }
